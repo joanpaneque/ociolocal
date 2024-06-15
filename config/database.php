99e3f6a2f9913c8771facc09e31206281e7e2d40
Joan Paneque Domingo <joanpd0@gmail.com>
2024-06-15 19:29:03 +0200

    Changes to make CI testing work

--- config/database.php
+++ config/database.php
@@ -34,6 +34,14 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
+        'testing' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('testing.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
