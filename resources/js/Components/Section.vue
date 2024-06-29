b6a8f645646c2bbd144f1c264483ac2b3b99876f
Joan Paneque Domingo <joanpd0@gmail.com>
2024-06-30 00:15:10 +0200

    Scroll elements

---
+++ resources/js/Components/Section.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    secondary: {
+        type: String,
+        required: false
+    }
+})
+</script>
+
+<template>
+    <section>
+        <div class="grid items-center justify-between mb-4 grid-cols-[auto,1fr]">
+            <h2 class="text-[20px] font-[700]">
+                {{ title }}
+            </h2>
+            <div class="text-[12px] leading-[1] text-right font-bold">
+                {{ secondary }}
+            </div>
+        </div>
+        <slot/>
+    </section>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
+import Section from '@/Components/Section.vue';
 
 
 function handleImageError() {
@@ -11,6 +12,7 @@ function handleImageError() {
 </script>
 
 <template>
+
     <Head title="Bienvenido" />
     <header class="bg-[url('/assets/welcome/header.jpg')] h-[177px] bg-cover bg-center relative mb-4">
         <div class="flex items-center justify-center h-full bg-black bg-opacity-70">
@@ -25,15 +27,35 @@ function handleImageError() {
         <div class="w-full h-[40px] bg-white absolute bottom-[-20px] rounded-[50%]"></div>
     </header>
     <main class="flex flex-col pr-4 pl-4">
-        <section>
-            <div class="grid items-center justify-between mb-4 grid-cols-[auto,1fr]">
-                <h2 class="text-[20px] font-[700]">
-                    Descuentos + TOP 🔥
-                </h2>
-                <div class="text-[14px] text-gray-500 leading-[1] text-right font-bold">
-                    02:35:13h restantes
-                </div>
+        <Section title="Descuentos + TOP 🔥" secondary="02:35:13h restantes">
+            <div class="flex gap-2 overflow-x-auto w-full">
+                <div class="w-5 h-5 bg-red-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-yellow-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-green-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-blue-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-purple-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-pink-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-yellow-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-green-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-red-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-yellow-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-green-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-blue-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-purple-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-pink-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-yellow-500 flex-shrink-0"></div>
+                <div class="w-5 h-5 bg-green-500 flex-shrink-0"></div>
             </div>
-        </section>
+        </Section>
+
+        <Section title="¿Qué te apetece?">
+
+        </Section>
+        <Section title="Encuentra un plan para ti">
+
+        </Section>
+        <Section title="Opiniones">
+
+        </Section>
     </main>
 </template>
