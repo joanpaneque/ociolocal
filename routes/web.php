cc4635deecf5c30d3b453b7e05a73e60d5c137ac
Joan Paneque Domingo <joanpd0@gmail.com>
2024-06-29 23:55:55 +0200

    Welcome page header + section

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+use App\Http\Controllers\IndexController;
+
+Route::get('/', [IndexController::class, 'index'])->name('index');
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
