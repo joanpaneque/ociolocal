b0dedfcd98ce09d7ba29c7322d02fc65bb995678
Joan Paneque Domingo <joanpd0@gmail.com>
2024-06-30 22:14:57 +0200

    Fixed inertia issue redirecting to Google OAuth

--- resources/js/Components/GoogleLogin.vue
+++ resources/js/Components/GoogleLogin.vue
@@ -4,10 +4,9 @@ import { router } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <button
-    @click="router.get(route('google.redirect'))"
-    class="text-[15px] h-[40px] w-full rounded-[10px] flex items-center justify-start relative border border-gray-500 px-3 py-6 mt-4">
+    <a :href="route('google.redirect')"
+        class="text-[15px] h-[40px] w-full rounded-[10px] flex items-center justify-start relative border border-gray-500 px-3 py-6 mt-4">
         <img src="/assets/icons/google_logo.png" alt="Google logo" class="h-[30px] w-[30px] mr-2">
         <div class="text-gray-500">Iniciar sesión con Google</div>
-    </button>
+    </a>
 </template>
