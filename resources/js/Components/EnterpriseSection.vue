78314cff085ff5ab6032f7b7e60583c742360373
Joan Paneque Domingo <joanpd0@gmail.com>
2024-07-01 01:42:57 +0200

    Added enterprise profile UI sections

---
+++ resources/js/Components/EnterpriseSection.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    href: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: false
+    }
+})
+</script>
+
+<template>
+    <div class="bg-gradient-to-r from-[#EC48C1] to-[#FED860] w-full h-[140px] rounded-[14px] p-[2px] flex items-center justify-center">
+        <div class="bg-white w-full h-full rounded-[13px] flex items-center justify-center flex-col px-[20px] gap-2">
+            <img :src="icon" alt="Icon" class="w-[60px] h-[60px]">
+            <span class="text-[15px] font-[600] text-center leading-4">{{ title }}</span>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { defineProps } from 'vue';
 import NavigationBar from '@/Components/NavigationBar.vue';
+import EnterpriseSection from '@/Components/EnterpriseSection.vue';
 
 const props = defineProps({
     user: {
@@ -11,8 +12,44 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex flex-col items-center">
-        <img :src="user.avatar" alt="User avatar" class="w-[100px] h-[100px] rounded-full mb-4 mt-4 border-2 border-gray-300">
+    <div class="px-[40px]">
+        <div class="flex flex-col items-center">
+            <img :src="user.avatar" alt="User avatar"
+                class="w-[100px] h-[100px] rounded-full mb-2 mt-4 border-2 border-gray-300">
+            <h1 class="text-[24px] font-[600] text-center mb-3">{{ user.name }}</h1>
+            <div v-if="user.enterprise"
+                class="text-[15px] text-white bg-gradient-to-r from-[#EC48C1] to-[#FED860] px-3 py-1 font-semibold rounded-2xl flex items-center gap-2">
+                <span>Enterprise</span>
+                <img src="/assets/icons/crown.svg" alt="Enterprise icon" class="invert w-[20px] h-[20px] inline-block">
+            </div>
+            <div v-else class="flex items-center flex-col gap-1">
+                <div
+                    class="text-[15px] w-fit text-white bg-gradient-to-r from-[#d8d8d8] to-[#9b9b9b] px-3 py-1 font-semibold rounded-2xl gap-2">
+                    Basic</div>
+                <div class="underline text-gray-600">Solicitar cuenta <span
+                        class="bg-clip-text text-transparent bg-gradient-to-r from-[#EC48C1] to-[#FED860]">enterprise</span>
+                </div>
+            </div>
+        </div>
+        <!-- :href="route('user.activities', user.id)" -->
+        <div class="rect-grid mt-4">
+            <EnterpriseSection v-if="user.enterprise" icon="/assets/icons/island_tropical.svg"
+                title="Mis actividades" />
+
+            <EnterpriseSection v-if="user.enterprise" icon="/assets/icons/euro.svg"
+                title="Ventas" />
+            <EnterpriseSection v-if="user.enterprise" icon="/assets/icons/scan_qr.svg"
+                title="Escanear entradas" />
+        </div>
     </div>
-    <NavigationBar/>
+    <NavigationBar />
 </template>
+
+
+<style scoped>
+.rect-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+    gap: 15px;
+}
+</style>
